ca7c48c19948ce9fb0a331225c26f0fbe9572e05
UlisesIslas <20193tn013@utez.edu.mx>
2022-11-21 21:29:17 -0600

    Funcionalidades faltantes

---
+++ js/detalles-recoleccion.js
@@ -0,0 +1,37 @@
+async function fetchUrl() {
+    const response = await fetch(`http://localhost:8000/api/recolectados/${new URLSearchParams(window.location.search).get('rec')}`, {
+        headers: { 
+            Accept: 'application/json',
+            Authorization: `Bearer ${localStorage.getItem('TOKEN')}`
+        },
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+}
+
+async function fetchInfo() {
+    const response = await fetch(`http://localhost:8000/api/recoleccion/detalles/${new URLSearchParams(window.location.search).get('rec')}`, {
+        headers: { 
+            Accept: 'application/json',
+        },
+    });
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return await response.json();
+}
+
+fetchUrl().then(data => {
+    let body = "";
+    Object.entries(data.data).forEach(([key, value]) => {
+        body += `<li class="list-group-item d-flex justify-content-between align-items-start"><div class="ms-2 me-auto"><div class="fw-bold">${value.alimento}</div>${value.categoria}</div></li>`;
+    });
+    document.getElementById('alimentos-seleccionados').innerHTML = body;
+})
+
+fetchInfo().then(data => {
+    document.getElementById('origen'). innerHTML = `<b>Almacen (Origen): </b>${data.data[0].cadena}`
+    document.getElementById('destino').innerHTML = `<b>Banco (Destino): </b>${data.data[0].banco}`
+})
--- js/listado_recolecciones.js
+++ js/listado_recolecciones.js
@@ -1,5 +1,4 @@
 const GET_RECOLECCIONES_URL = 'http://localhost:8000/api/recolecciones-usuario';
-const GET_CADENA_COMERCIAL_URL = 'http://localhost:8000/api/cadenaComercial/show/';
 async function fetchUrl(url) {
     const response = await fetch(url, {
         headers: { 
@@ -13,26 +12,52 @@ async function fetchUrl(url) {
     return await response.json();
 }
 
-const STATUS_ONE_BADGE = '<span class="badge bg-warning">Pendiente</span></div><div class="col-3 col-md-2"><a href="/pages/pendiente-detalles.html" class="btn btn-info btn-sm">Recolectar</a></div></div></div></div>';
-const STATUS_TWO_BADGE = '<span class="badge bg-info">En camino</span></div><div class="col-3 col-md-2"><a href="" class="btn btn-primary btn-sm">Entregado</a></div></div></div></div>';
-const STATUS_THREE_BADGE = '<span class="badge bg-success">Realizada</span></div><div class="col-3 col-md-2"><a href="/pages/detalles-recoleccion.html" class="btn btn-secondary btn-sm">Detalles</a></div></div></div></div>';
-
 fetchUrl(GET_RECOLECCIONES_URL).then(data => {
     let body = '<div class = "col-12"><div class="row">'
-    let i = 0;
     Object.entries(data.data).forEach(([key, value]) => {
-        body += `<div class="card mt-3"><div class="card-body"><div class="row"><div class="col-6 col-md-8 text-truncate" id="cc${value.id}"></div><div class="col-3 col-md-2">${value.status === 1 ? STATUS_ONE_BADGE : value.status === 2 ? STATUS_TWO_BADGE : STATUS_THREE_BADGE}`
-        getCadena(value.almacen.cadena_comercial_id, value.id);
-        console.log(value.almacen.cadena_comercial_id);
-        i++;
+        body += `<div class="card mt-3"><div class="card-body"><div class="row"><div class="col-6 col-md-8 text-truncate" id="cc${value.id}">${value.nombre}</div><div class="col-3 col-md-2">${value.status === 1 ? `<span class="badge bg-warning">Pendiente</span></div><div class="col-3 col-md-2"><a href="/pages/pendiente-detalles.html?alm=${value.almacen}&rec=${value.id}" class="btn btn-info btn-sm">Recolectar</a></div></div></div></div>` : value.status === 2 ? `<span class="badge bg-info">En camino</span></div><div class="col-3 col-md-2"><button data-id="${value.id}" class="btn btn-primary btn-sm entregar">Entregado</button></div></div></div></div>` : `<span class="badge bg-success">Realizada</span></div><div class="col-3 col-md-2"><a href="/pages/detalles-recoleccion.html?rec=${value.id}" class="btn btn-secondary btn-sm">Detalles</a></div></div></div></div>`}`
     });
     document.getElementById('listado').innerHTML = body;
-})
 
-async function getCadena (id, idObj) {
-    await fetchUrl(GET_CADENA_COMERCIAL_URL + id).then(data => {
-        console.log("FUN: " + data.data.nombre);
-        document.getElementById(`cc${idObj}`).innerHTML = data.data.nombre;
-        return data.data;
-    })
-}
+    document.querySelectorAll('.entregar').forEach(x => {
+        x.addEventListener('click', async function() {
+            const response = await fetch(`http://localhost:8000/api/actualizar-estatus-recoleccion/${x.dataset.id}`, {
+                method: "GET",
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${localStorage.getItem('TOKEN')}`
+                },
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            } else {
+                console.log("Si");
+                fetchUrl(GET_RECOLECCIONES_URL).then(data => {
+                    let body = '<div class = "col-12"><div class="row">'
+                    Object.entries(data.data).forEach(([key, value]) => {
+                        body += `<div class="card mt-3"><div class="card-body"><div class="row"><div class="col-6 col-md-8 text-truncate" id="cc${value.id}">${value.nombre}</div><div class="col-3 col-md-2">${value.status === 1 ? `<span class="badge bg-warning">Pendiente</span></div><div class="col-3 col-md-2"><a href="/pages/pendiente-detalles.html?alm=${value.almacen}&rec=${value.id}" class="btn btn-info btn-sm">Recolectar</a></div></div></div></div>` : value.status === 2 ? `<span class="badge bg-info">En camino</span></div><div class="col-3 col-md-2"><button data-id="${value.id}" class="btn btn-primary btn-sm entregar">Entregado</button></div></div></div></div>` : STATUS_THREE_BADGE}`
+                    });
+                    document.getElementById('listado').innerHTML = body;
+                
+                    document.querySelectorAll('.entregar').forEach(x => {
+                        x.addEventListener('click', async function() {
+                            const response = await fetch(`http://localhost:8000/api/actualizar-estatus-recoleccion/${x.dataset.id}`, {
+                                method: "GET",
+                                headers: {
+                                    Accept: 'application/json',
+                                    Authorization: `Bearer ${localStorage.getItem('TOKEN')}`
+                                },
+                            });
+                            if (!response.ok) {
+                                throw new Error(`HTTP error! status: ${response.status}`);
+                            } else {
+                                console.log("Si");
+                                fetchUrl(GET_RECOLECCIONES_URL)
+                            }
+                        });
+                    });
+                });
+            }
+        });
+    });
+});
